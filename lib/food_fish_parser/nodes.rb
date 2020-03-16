8b03302550d598b2d0116ba7f29d6a7198fe49f7
wvengen <dev-ruby@willem.engen.nl>
2020-03-16 16:16:49 +0100

    Add aquaculture (#1)

--- lib/food_fish_parser/nodes.rb
+++ lib/food_fish_parser/nodes.rb
@@ -29,7 +29,9 @@ module FoodFishParser
         {
           names: to_a_deep(self, FishNameNode).map(&:to_h),
           catch_areas: to_a_deep(self, CatchAreaNode).map(&:to_h),
-          catch_methods: to_a_deep(self, CatchMethodNode).map(&:to_h)
+          catch_methods: to_a_deep(self, CatchMethodNode).map(&:to_h),
+          aquaculture_areas: to_a_deep(self, AquacAreaNode).map(&:to_h),
+          aquaculture_methods: to_a_deep(self, AquacMethodNode).map(&:to_h),
         }
       end
     end
@@ -52,18 +54,35 @@ module FoodFishParser
       def to_h
         {
           text: area.text_value,
-          fao_codes: to_a_deep(self, CatchAreaFAOCodeNode).map(&:text_value)
+          fao_codes: to_a_deep(self, FaoAreaCodeNode).map(&:text_value)
         }
       end
     end
 
-    module CatchAreaFAOCodeNode; end
+    module FaoAreaCodeNode; end
 
     module CatchMethodNode
+      def to_h
+        {
+          text: self.text_value
+        }
+      end
+    end
+
+    module AquacAreaNode
       include SyntaxNodeAdditions
       def to_h
         {
-          text: method.text_value
+          text: area.text_value,
+          fao_codes: to_a_deep(self, FaoAreaCodeNode).map(&:text_value)
+        }
+      end
+    end
+
+    module AquacMethodNode
+      def to_h
+        {
+          text: self.text_value
         }
       end
     end
