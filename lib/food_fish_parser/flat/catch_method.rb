d8554ba9eccedd77bb6b98d614e893b4cbc2d45e
wvengen <dev-ruby@willem.engen.nl>
2020-05-07 10:59:01 +0200

    Expand NL catch methods according to EU law
    Source:
    - https://doi.org/10.2771/83592
    - https://ec.europa.eu/fisheries/cfp/market/consumer-information_nl

--- lib/food_fish_parser/flat/catch_method.rb
+++ lib/food_fish_parser/flat/catch_method.rb
@@ -5,26 +5,34 @@ module FoodFishParser
       REGEX = /
         \b
         (
-          trawlnet(?:ten)? |
-          (?:pelagische\s+)? trawler (?:\s*,?\s*pelagisch)? |
-          pelagische\s+visserij |
-          haken\s+en\s+lijnen |
-          (?:machinale\s+)? handlijnen |
-          ring-\s+en\s+kruisnet(?:ten)? |
-          ringnet(?:ten)? |
-          kruisnet(?:ten)? |
-          sleeplijn(?:en)? |
-          kieuwnet(?:ten)? |
-          (?:soortgelijke|vergelijkbare)\s+net(?:ten)? |
-          (?:zwevende\s+)? (?:bodem)?(?:otter)?trawls? |
-          (?:deense\s+)? zegens? |
-          ringzegens? |
-          hengels?\s+en\s+lijn(?:en)? |
-          met\s+de\s+hengel |
-          met\s+lijnen |
-          vallen |
-          purse\s*seiner
-        ) (?:visserij)?
+          (?:
+            trawlnet(?:ten)? |
+            (?:pelagische\s+)? trawler (?:\s*,?\s*pelagisch)? |
+            pelagische\s+visserij |
+            haken\s+en\s+lijnen |
+            (?:machinale\s+)? handlijnen |
+            (?:ring-|ringnet|ringetten)\s+en\s+kruisnet(?:ten)? |
+            ringnet(?:ten)? |
+            kruisnet(?:ten)? |
+            sleeplijn(?:en)? |
+            sleepnet(?:en)? |
+            kieuwnet(?:ten)? |
+            lijn-?gevangen |
+            (?:soortgelijke|vergelijkbare)\s+net(?:ten)? |
+            (?:zwevende\s+)? (?:bodem)?(?:otter)?trawls? |
+            (?:deense\s+)? zegens? |
+            ringzegens? |
+            hengels?\s+en\s+lijn(?:en)? |
+            met\s+de\s+hengel |
+            met\s+lijnen |
+            korven\s+en\+vallen |
+            vallen |
+            dreggen |
+            purse\s*seiner
+          )
+          (?:\s+en\s+soortgelijke\s+netten)?
+        )
+        (?:visserij)?
         \b
       /ix
 
